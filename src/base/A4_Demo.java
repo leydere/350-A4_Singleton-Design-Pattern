ba989334ad29581c628fc51c401a203177ae33f6
Eric Leyder <leydere@ferris.edu>
2020-03-12 15:03:46 -0400

    Finished
    Got a little into the groove of solving unknowns until I realized I was done with assignment and had not made an commits

--- src/base/A4_Demo.java
+++ src/base/A4_Demo.java
@@ -4,8 +4,24 @@ public class A4_Demo {
 
 	 public static void main(String args[]){  
 
-		  
+		 Singleton mySingleton = Singleton.getSingleton();
+		 System.out.println("The application has started.  Please allow a few seconds for next line to print.  Expecting roughly 4000 milliseconds.");
+		 
+		 try {
+			    Thread.sleep(4000);
+			} catch (InterruptedException ie) {
+			    Thread.currentThread().interrupt();
+			}
+		 
+		 System.out.println(mySingleton.getRunningTime());
+		 System.out.println("The system is going to run another 6 seconds and the print time in hours represented in decimal fashion.  Expecting roughly 0.002778 hours");
 
+		 try {
+			    Thread.sleep(6000);
+			} catch (InterruptedException ie) {
+			    Thread.currentThread().interrupt();
+			}
+		 System.out.println(mySingleton.getRunningTimeinHours());
 	 }  	
 	
 }
--- src/base/Singleton.java
+++ src/base/Singleton.java
@@ -1,16 +1,36 @@
 package base;
 
 public class Singleton {
-	 
-	 private static Singleton obj=new Singleton();//Early, instance will be created at load time 
-	 
-	 private Singleton(){}  
-	   
-	 public static Singleton getSingleton(){  
-	  return obj;  
-	 }  
-	  
-	 public void doSomething(){  
-	 //write your code  
-	 }  
+
+	private static Singleton obj = new Singleton();// Early, instance will be created at load time
+	private long startedTime;
+
+	private Singleton() {
+		startTimer();
+	}
+
+	public static Singleton getSingleton() {
+		return obj;
+	}
+
+	public void doSomething() {
+		// write your code
+	}
+
+	public String getRunningTime() {
+
+		long elapsedTime = System.currentTimeMillis() - startedTime;
+		return "The current running time in milliseconds is: " + elapsedTime;
+	}
+
+	public void startTimer() {
+		startedTime = System.currentTimeMillis();
+	}
+
+	public String getRunningTimeinHours() {
+
+		long elapsedTime = System.currentTimeMillis() - startedTime;
+		float floatTime = (float)elapsedTime / 3600000;
+		return "The current running time in hours is: " + floatTime;
+	}
 }
